68c8aab98cf22d0524fb0e8aa28fe1bcf6aaf298
jamili <jamili.amaral@estudante.ifms.edu.br>
2023-11-30 22:15:21 -0400

    fazendo geolocalizacao

--- f1/js/db.js
+++ f1/js/db.js
@@ -25,6 +25,7 @@ window.addEventListener('DOMContentLoaded', async event => {
     criarDB();
     document.getElementById('btnCadastro').addEventListener('click', adicionarPistaFormula1);
     document.getElementById('btnCarregar').addEventListener('click', listarPistasFormula1);
+    document.getElementById('btnBuscar').addEventListener('click', buscarPistaFormula1);
 });
 
 async function adicionarPistaFormula1() {
@@ -34,15 +35,20 @@ async function adicionarPistaFormula1() {
     let dataGrandePremio = document.getElementById("dataGrandePremio").value;
     let estiloPista = document.getElementById("estiloPista").value;
     let licencaFIA = document.getElementById("licencaFIA").value;
+    let latitude = document.getElementById("latitude").value;
+    let longitude = document.getElementById("longitude").value;
 
     if (!nomePista || !pais || !quantidadeCurvas || !dataGrandePremio || !estiloPista || !licencaFIA) {
         console.log('Preencha todos os campos obrigatórios.');
         return;
     }
-
+    if (!latitude || !longitude) {
+        console.log('Preencha os campos de latitude e longitude.');
+        return;
+    }
     const tx = await db.transaction('pistaF1', 'readwrite');
     const store = tx.objectStore('pistaF1');
-
+    
     try {
         await store.add({
             nomePista: nomePista,
@@ -50,15 +56,39 @@ async function adicionarPistaFormula1() {
             quantidadeCurvas: quantidadeCurvas,
             dataGrandePremio: dataGrandePremio,
             estiloPista: estiloPista,
-            licencaFIA: licencaFIA
+            licencaFIA: licencaFIA,
+            latitude: latitude, 
+            longitude: longitude 
         });
         await tx.done;
         limparCampos();
+        exibirNoMapa(parseFloat(latitude), parseFloat(longitude));
         console.log('Pista de Fórmula 1 cadastrada com sucesso!');
     } catch (error) {
         console.error('Erro ao cadastrar pista de Fórmula 1:', error);
         tx.abort();
     }
+}
+function exibirNoMapa(latitude, longitude) {
+    if (isNaN(latitude) || isNaN(longitude)) {
+        console.error('Coordenadas inválidas.');
+        return;
+    }
+
+    const myLatlng = new google.maps.LatLng(latitude, longitude);
+    const mapOptions = {
+        zoom: 10,
+        center: myLatlng,
+    };
+
+    const map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+    const marker = new google.maps.Marker({
+        position: myLatlng,
+        map: map,
+        title: 'Local da Pista de Fórmula 1',
+    });
+    document.getElementById('map').style.display = 'block';
 }
 
 async function listarPistasFormula1() {
@@ -78,11 +108,19 @@ async function listarPistasFormula1() {
                     <p>Quantidade de Curvas: ${pista.quantidadeCurvas}</p>
                     <p>Data do Grande Prêmio: ${pista.dataGrandePremio}</p>
                     <p>Grau de Licença FIA: ${pista.licencaFIA}</p>
-                </div>`;
+                    <button class="btnMostrarMapa" data-latitude="${pista.latitude}" data-longitude="${pista.longitude}">Mostrar Mapa</button>
+                    </div>`;
         });
         listagem(divLista.join(' '));
+        document.querySelectorAll('.btnMostrarMapa').forEach(button => {
+            button.addEventListener('click', function () {
+              const latitude = parseFloat(this.getAttribute('data-latitude'));
+              const longitude = parseFloat(this.getAttribute('data-longitude'));
+              exibirNoMapa(latitude, longitude);
+            });
+        });
+      }
     }
-}
 
 function limparCampos() {
     document.getElementById("nomePista").value = '';
@@ -91,6 +129,37 @@ function limparCampos() {
     document.getElementById("dataGrandePremio").value = '';
     document.getElementById("estiloPista").value = '';
     document.getElementById("licencaFIA").value = '';
+}
+
+async function buscarPistaFormula1() {
+    const nomePistaBusca = document.getElementById('inputBuscar').value;
+    if (!nomePistaBusca) {
+        console.log('Nome da pista não fornecido para busca.');
+        return;
+    }
+
+    const tx = await db.transaction('pistaF1', 'readonly');
+    const store = tx.objectStore('pistaF1');
+    const index = store.index('id');
+
+    try {
+        const pistaEncontrada = await index.get(nomePistaBusca);
+        if (pistaEncontrada) {
+            const divPista = `<div class="item">
+                <p>Nome da Pista: ${pistaEncontrada.nomePista}</p>
+                <p>País: ${pistaEncontrada.pais}</p>
+                <p>Quantidade de Curvas: ${pistaEncontrada.quantidadeCurvas}</p>
+                <p>Data do Grande Prêmio: ${pistaEncontrada.dataGrandePremio}</p>
+                <p>Grau de Licença FIA: ${pistaEncontrada.licencaFIA}</p>
+            </div>`;
+            listagem(divPista);
+        } else {
+            console.log(`Pista com o nome '${nomePistaBusca}' não encontrada.`);
+            listagem(''); 
+        }
+    } catch (error) {
+        console.error('Erro ao buscar pista de Fórmula 1:', error);
+    }
 }
 
 function listagem(text) {
